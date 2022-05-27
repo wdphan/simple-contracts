d8751b377f9a1cef02deb65fa6a2e639b6793a6f
"William Phan" <"willdphan@gmail.com">
2022-05-27 19:17:31 -0400

    added simple multisend

---
+++ contracts/SimpleMultSend.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.19 <0.9.0;
+
+contract myGame {
+
+    uint public playerCount = 0;
+    uint public pot = 0;
+
+    address public dealer;
+
+    Player[] public playersInGame;
+
+    mapping (address => Player) public players;
+
+    enum Level {Novice, Intermediate, Advanced}
+    
+    struct Player {
+        address playerAddress;
+        Level playerLevel;
+        string firstName;
+        string lastName;
+        uint createdTime;
+    }
+
+    constructor(){
+        dealer = msg.sender;
+    }
+
+    function addPlayer(string memory firstName, string memory lastName) private {
+        Player memory newPlayer = Player(msg.sender, Level.Novice, firstName, lastName, block.timestamp);
+        players[msg.sender] = newPlayer;
+        playersInGame.push(newPlayer);
+    }
+
+    function getPlayerLevel(address playerAddress) private view returns (Level) {
+        Player storage player = players[playerAddress];
+        return player.playerLevel;
+    }
+
+    function changePlayerLevel (address playerAddress) private {
+        Player storage player = players[playerAddress];
+        if (block.timestamp >= player.createdTime + 20) {
+            player.playerLevel = Level.Intermediate;
+        }
+    }
+
+    function joinGame(string memory firstName, string memory lastName) payable public {
+        require(msg.value == 25 ether, "The joining fee is 25 ether");
+        if (payable(dealer).send(msg.value)) {
+            addPlayer(firstName, lastName);
+            playerCount += 1;
+            pot += 25;
+        }
+    }
+
+    function payOutWinners(address loserAddress) payable public {
+        require(msg.sender == dealer, "Only the dealer can pay out the winners.");
+        require(msg.value == pot * (1 ether));
+        uint payoutPerWinner = msg.value / (playerCount - 1);
+        for (uint i=0; i<playersInGame.length; i++) {
+            address currentPlayerAddress = playersInGame[i].playerAddress;
+            if (currentPlayerAddress != loserAddress)
+                payable(currentPlayerAddress).transfer(payoutPerWinner);
+            }
+        }
+    }
